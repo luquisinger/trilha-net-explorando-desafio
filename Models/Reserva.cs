4a414c41bf50bed86f2a0261bd545b054e21b0c5
Matheus <matheus.779@hotmail.com>
2024-05-22 17:42:36 -0300

    desafio resolvido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -63,38 +63,5 @@ namespace DesafioProjetoHospedagem.Models
             return valor;
         }
     }
-	public class Pessoa
-	{
-		public Pessoa() { }
-
-		public Pessoa(string nome)
-		{
-			Nome = nome;
-		}
-
-		public Pessoa(string nome, string sobrenome)
-		{
-			Nome = nome;
-			Sobrenome = sobrenome;
-		}
-
-		public string Nome { get; set; }
-		public string Sobrenome { get; set; }
-		public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
-	}
-	public class Suite
-	{
-		public Suite() { }
-
-		public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
-		{
-			TipoSuite = tipoSuite;
-			Capacidade = capacidade;
-			ValorDiaria = valorDiaria;
-		}
-
-		public string TipoSuite { get; set; }
-		public int Capacidade { get; set; }
-		public decimal ValorDiaria { get; set; }
-	}
+	
 }
