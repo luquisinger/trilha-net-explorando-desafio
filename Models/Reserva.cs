9bfcea1378f54626afe74731b13502457d698940
Matheus <matheus.779@hotmail.com>
2024-05-22 17:42:12 -0300

    Desafio resolvido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,5 +1,6 @@
 namespace DesafioProjetoHospedagem.Models
 {
+    
     public class Reserva
     {
         public List<Pessoa> Hospedes { get; set; }
@@ -15,16 +16,19 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
+            Suite suite = new Suite();
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (hospedes.Count <= suite.Capacidade)
             {
+                Suite = suite;
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new Exception("capacidade é menor que o número de hóspedes recebidos");
             }
         }
 
@@ -37,7 +41,8 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
@@ -45,16 +50,51 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >=10)
             {
-                valor = 0;
+                valor = valor - (valor * 0.1);
             }
 
             return valor;
         }
     }
+	public class Pessoa
+	{
+		public Pessoa() { }
+
+		public Pessoa(string nome)
+		{
+			Nome = nome;
+		}
+
+		public Pessoa(string nome, string sobrenome)
+		{
+			Nome = nome;
+			Sobrenome = sobrenome;
+		}
+
+		public string Nome { get; set; }
+		public string Sobrenome { get; set; }
+		public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+	}
+	public class Suite
+	{
+		public Suite() { }
+
+		public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
+		{
+			TipoSuite = tipoSuite;
+			Capacidade = capacidade;
+			ValorDiaria = valorDiaria;
+		}
+
+		public string TipoSuite { get; set; }
+		public int Capacidade { get; set; }
+		public decimal ValorDiaria { get; set; }
+	}
 }
